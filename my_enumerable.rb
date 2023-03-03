d5050cc60bd377a0598fd9ba2b3dbe637547f411
PrantoshB <pbiswas1007@gmail.com>
2023-03-03 21:50:49 +0530

    Solve Errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,19 +3,20 @@ module MyEnumerable
     each do |item|
       return false unless block.call(item)
     end
+    true
   end
 
   def any?(&block)
     each do |item|
       return true if block.call(item)
     end
+    false
   end
 
   def filter?(&block)
     result = []
     each do |item|
-      result << item if block.call(item)
+      result << item if block.call(item) == true
     end
-    result
   end
 end
